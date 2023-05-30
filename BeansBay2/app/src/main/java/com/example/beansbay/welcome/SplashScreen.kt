c3569c5aa3d5749a4c8c98f27c3ced6f5d928c55
isroabdillah <isroabdillah.dev@gmail.com>
2023-05-30 22:40:00 +0700

    main

---
+++ BeansBay2/app/src/main/java/com/example/beansbay/welcome/SplashScreen.kt
@@ -0,0 +1,12 @@
+package com.example.beansbay.welcome
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.beansbay.R
+
+class SplashScreen : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash_screen)
+    }
+}
